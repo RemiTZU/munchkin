6e0e8f89ad0db0ec4cf5a56fa3d9e662e11bbb22
Léon <63704685+Leon-afk1@users.noreply.github.com>
2023-12-26 22:10:55 +0100

    Update MunchkinGame.java
    gestion d'un tour

--- MunchkinGame.java
+++ MunchkinGame.java
@@ -1,36 +1,24 @@
+import java.util.*;
 public class MunchkinGame {
     public static void main(String[] args) {
+        // Demander le nombre de joueurs
+        int nbJoueurs = demanderNombreJoueurs();
 
-        // Création d'un paquet
-        Paquet paquet = new Paquet();
+        // Création d'un nouveau tour avec le nombre de joueurs
+        Tour tour = new Tour(nbJoueurs);
 
-        paquet.creerCartes();
-
-        // Mélange du paquet
-        paquet.melanger();
-
-        Joueur joueur = new Joueur();
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("PORTE"));
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("PORTE"));
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("PORTE"));
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("PORTE"));
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("TRESORS"));
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("TRESORS"));
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("TRESORS"));
-        joueur.piocherCarte(paquet.tirerCarteAleatoire("TRESORS"));
-
-        joueur.afficherJoueur();
-
-        joueur.jouerCarte(joueur.getMain().get(0));
-        joueur.jouerCarte(joueur.getMain().get(0));
-        joueur.jouerCarte(joueur.getMain().get(0));
-        joueur.jouerCarte(joueur.getMain().get(0));
-        joueur.jouerCarte(joueur.getMain().get(0));
-        joueur.jouerCarte(joueur.getMain().get(0));
-        joueur.jouerCarte(joueur.getMain().get(0));
-        joueur.jouerCarte(joueur.getMain().get(0));
-
-        joueur.afficherJoueur();
+        // Commencer le tour
+        tour.tour();
     }
 
+    private static int demanderNombreJoueurs() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Entrez le nombre de joueurs : ");
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Veuillez entrer un nombre valide.");
+            return demanderNombreJoueurs();
+        }
+    }
 }
